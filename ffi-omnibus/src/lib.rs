da0fe7f0f9e16cfdeaa417c2bf2716a410134c49
p6steve <p6steve@furnival.net>
2022-05-28 14:26:07 +0000

    all Rust FFI Omnibus done (1 issue)

--- ffi-omnibus/src/lib.rs
+++ ffi-omnibus/src/lib.rs
@@ -1,41 +1,112 @@
-use std::ffi::*; //{CStr, CString,}
-use std::collections::HashMap;
-use std::cell::RefCell;
 use libc::c_char;
+use libc::size_t;
+use std::ffi::*; //{CStr, CString,}
+use std::iter;
+use std::slice;
+use std::convert::From;
+use std::collections::HashMap;
 
-fn str_in(i_string: *const c_char) -> String {
-    unsafe {
-        CStr::from_ptr(i_string).to_string_lossy().into_owned()
-    }
-}
-#[no_mangle]
-pub extern "C" fn str2rust(string: *const c_char) {
-    println!("{:?}", str_in(string));
-}
-
-//viz. https://metacpan.org/pod/FFI::Platypus::Lang::Rust#returning-strings
-fn str_out(o_string: &str) -> *const u8 {
-    thread_local! {
-        static KEEP: RefCell<Option<CString>> = RefCell::new(None);
-    }
- 
-    let c_string = CString::new(o_string).unwrap();
-    let ptr = c_string.as_ptr();
-    KEEP.with(|k| {
-        *k.borrow_mut() = Some(c_string);
-    });
- 
-    ptr
-}
-#[no_mangle]
-pub extern "C" fn str2raku() -> *const u8 {
-    str_out("yo")
-}
+// Rust FFI Omnibus: Integers
+// http://jakegoulding.com/rust-ffi-omnibus/integers/
 
 #[no_mangle]
-pub extern "C" fn add(a:i32, b:i32) -> i32 {
+pub extern "C" fn addition(a:i32, b:i32) -> i32 {
     a+b 
 }
+
+// Rust FFI Omnibus: String Arguments
+// http://jakegoulding.com/rust-ffi-omnibus/string-arguments/
+
+#[no_mangle]
+pub extern "C" fn how_many_characters(s: *const c_char) -> u32 {
+    let c_str = unsafe {
+        assert!(!s.is_null());
+
+        CStr::from_ptr(s)
+    };
+
+    let r_str = c_str.to_str().unwrap();
+    r_str.chars().count() as u32
+}
+
+// Rust FFI Omnibus: String Return Values
+// http://jakegoulding.com/rust-ffi-omnibus/string-return-values/
+
+#[no_mangle]
+pub extern "C" fn theme_song_generate(length: u8) -> *mut c_char {
+    let mut song = String::from("💣 ");
+    song.extend(iter::repeat("na ").take(length as usize));
+    song.push_str("Batman! 💣");
+
+    let c_str_song = CString::new(song).unwrap();
+    c_str_song.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn theme_song_free(s: *mut c_char) {
+    unsafe {
+        if s.is_null() {
+            return;
+        }
+        CString::from_raw(s)
+    };
+}
+
+// Rust FFI Omnibus: Slice Arguments
+// http://jakegoulding.com/rust-ffi-omnibus/slice-arguments/
+
+#[no_mangle]
+pub extern "C" fn sum_of_even(n: *const u32, len: size_t) -> u32 {
+    let numbers = unsafe {
+        assert!(!n.is_null());
+
+        slice::from_raw_parts(n, len as usize)
+    };
+
+    numbers
+        .iter()
+        .filter(|&v| v % 2 == 0)
+        .sum()
+}
+
+// Rust FFI Omnibus: Tuples 
+// http://jakegoulding.com/rust-ffi-omnibus/tuples/
+
+// A Rust function that accepts a tuple
+fn flip_things_around_rust(tup: (u32, u32)) -> (u32, u32) {
+    let (a, b) = tup;
+    (b + 1, a - 1)
+}
+
+// A struct that can be passed between C and Rust
+#[repr(C)]
+pub struct Tuple {
+    x: u32,
+    y: u32,
+}
+
+// Conversion functions
+impl From<(u32, u32)> for Tuple {
+    fn from(tup: (u32, u32)) -> Tuple {
+        Tuple { x: tup.0, y: tup.1 }
+    }
+}
+
+impl From<Tuple> for (u32, u32) {
+    fn from(tup: Tuple) -> (u32, u32) {
+        (tup.x, tup.y)
+    }
+}
+
+// The exported C method
+#[no_mangle]
+//pub extern "C" fn flip_things_around(tup: Tuple) {
+pub extern "C" fn flip_things_around(tup: Tuple) -> Tuple {
+    flip_things_around_rust(tup.into()).into()
+}
+
+// Rust FFI Omnibus: Objects
+// http://jakegoulding.com/rust-ffi-omnibus/objects/
 
 pub struct ZipCodeDatabase {
     population: HashMap<String, u32>,
