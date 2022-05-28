7834fba6e8a8264c4edd053e411e87fcaea9370b
p6steve <p6steve@furnival.net>
2022-05-28 12:43:59 +0000

    add ZipCodeDataBase

--- ffi-omnibus.raku
+++ ffi-omnibus.raku
@@ -8,6 +8,37 @@ str2rust("hey");
 sub str2raku() returns Pointer[Str] is native($n-path) {*};
 say str2raku.deref;
 
+class ZipCodeDatabase is repr('CPointer') {
+    sub zip_code_database_new() returns ZipCodeDatabase is native($n-path) { * }
+    sub zip_code_database_free(ZipCodeDatabase)         is native($n-path) { * }
+    sub zip_code_database_populate(ZipCodeDatabase)     is native($n-path) { * }
+    sub zip_code_database_population_of(ZipCodeDatabase, Str is encoded('utf8'))
+                                         returns uint32 is native($n-path) { * }
+
+    method new {
+        zip_code_database_new
+    }
+
+    submethod DESTROY {        # Free data when the object is garbage collected.
+        zip_code_database_free(self);
+    }
+
+    method populate {
+        zip_code_database_populate(self)
+    }
+
+    method population_of( Str \zip ) {
+        zip_code_database_population_of(self, zip);
+    }
+}
+
+my \database = ZipCodeDatabase.new;
+    database.populate;
+
+my \pop1 = database.population_of('90210');
+my \pop2 = database.population_of('20500');
+say pop1 - pop2;
+
 sub add (int32, int32) returns int32 is native($n-path) { * };  
 say add(1, 2); 
 
--- ffi-omnibus/src/lib.rs
+++ ffi-omnibus/src/lib.rs
@@ -36,3 +36,68 @@ pub extern "C" fn str2raku() -> *const u8 {
 pub extern "C" fn add(a:i32, b:i32) -> i32 {
     a+b 
 }
+
+pub struct ZipCodeDatabase {
+    population: HashMap<String, u32>,
+}
+
+impl ZipCodeDatabase {
+    fn new() -> ZipCodeDatabase {
+        ZipCodeDatabase {
+            population: HashMap::new(),
+        }
+    }
+
+    fn populate(&mut self) {
+        for i in 0..100_000 {
+            let zip = format!("{:05}", i);
+            self.population.insert(zip, i);
+        }
+    }
+
+    fn population_of(&self, zip: &str) -> u32 {
+        self.population.get(zip).cloned().unwrap_or(0)
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zip_code_database_new() -> *mut ZipCodeDatabase {
+    Box::into_raw(Box::new(ZipCodeDatabase::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn zip_code_database_free(ptr: *mut ZipCodeDatabase) {
+    if ptr.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zip_code_database_populate(ptr: *mut ZipCodeDatabase) {
+    let database = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+    database.populate();
+}
+
+#[no_mangle]
+pub extern "C" fn zip_code_database_population_of(
+    ptr: *const ZipCodeDatabase,
+    zip: *const c_char,
+) -> u32 {
+    let database = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+    let zip = unsafe {
+        assert!(!zip.is_null());
+        CStr::from_ptr(zip)
+    };
+    let zip_str = zip.to_str().unwrap();
+    database.population_of(zip_str)
+}
+
